b29c0ed4148c0d74a009102daed1b323c9b5f929
Duncan Tasker <tasatree@gmail.com>
2022-10-31 09:59:44 -0400

    create dummy routes

--- app/routes.py
+++ app/routes.py
@@ -3,3 +3,37 @@ from app import app
 @app.route('/')
 def index():
     return "Hello, world!"
+
+@app.route('/artist/<name>')
+def artist(name):
+    return "Hello, %s!" % name
+
+@app.route('/artistlist')
+def artistlist():
+    return "Hello, world!"
+
+app.route('/song/<name>')
+def song(name):
+    return "Hello, %s!" % name
+
+app.route('/songlist')
+def songlist():
+    return "Hello, world!"
+
+app.route('album/<name>')
+def album(name):
+    return "Hello, %s!" % name
+
+app.route('/mystuff/artists')
+def mystuff():
+    return "Hello, world!"
+
+app.route*('/mystuff/songs')
+def songs():
+    return "Hello, world!"
+
+
+
+app.route('recommended')
+def recommended():
+    return "Hello, world!"
