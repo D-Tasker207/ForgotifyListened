598520be0e652f2b91cdc236b10d16411cd38b30
vconrad02 <99766657+vconrad02@users.noreply.github.com>
2022-10-31 20:26:00 -0400

    Worked on html pages and routes

--- app/routes.py
+++ app/routes.py
@@ -1,9 +1,10 @@
 from app import app
+from flask import render_template, redirect, url_for
 
 @app.route('/')
-@app.route('/home')
+@app.route('/index')
 def index():
-    return "Hello, world!"
+    return "Hello, World!"
 
 @app.route('/artist/<name>')
 def artist(name):
@@ -13,35 +14,39 @@ def artist(name):
 def artistlist():
     return "Hello, world!"
 
-app.route('/song/<name>')
+@app.route('/song/<name>')
 def song(name):
     return "Hello, %s!" % name
 
-app.route('/songlist')
+@app.route('/songlist')
 def songlist():
     return "Hello, world!"
 
-app.route('album/<name>')
+@app.route('album/<name>')
 def album(name):
     return "Hello, %s!" % name
 
-app.route('/mystuff/artists')
+@app.route('/mystuff/artists')
 def mystuff():
     return "Hello, world!"
 
-app.route('/mystuff/songs')
+@app.route('/mystuff/songs')
 def songs():
     return "Hello, world!"
 
-app.route('/recommended')
+@app.route('/recommended')
 def recommended():
     return "Hello, world!"
 
-app.route('/contact')
+@app.route('/contact')
 def contact():
     return "Hello, world!"
 
 @app.route('/login')
 def login():
+    return "Hello, world!"
+
+@app.route('/logout')
+def logout():
     return "Hello, world!"
 
